55425600404534d1b20178ea64ab068b938d15fc
cdar <cdar07@gmail.com>
2026-05-07 17:35:30 +0200

    2023 day 9 star 1

---
+++ src/advent_of_code/year_2023/day_9/star_1/algo.py
@@ -0,0 +1,149 @@
+"""
+You pull out your handy Oasis And Sand Instability Sensor and analyze your surroundings. The OASIS
+produces a report of many values and how they are changing over time (your puzzle input). Each line
+in the report contains the history of a single value. For example:
+
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+
+To best protect the oasis, your environmental report should include a prediction of the next value
+in each history. To do this, start by making a new sequence from the difference at each step of your
+history. If that sequence is not all zeroes, repeat this process, using the sequence you just
+generated as the input sequence. Once all of the values in your latest sequence are zeroes, you can
+extrapolate what the next value of the original history should be.
+
+In the above dataset, the first history is 0 3 6 9 12 15. Because the values increase by 3 each
+step, the first sequence of differences that you generate will be 3 3 3 3 3. Note that this sequence
+has one fewer value than the input sequence because at each step it considers two numbers from the
+input. Since these values aren't all zero, repeat the process: the values differ by 0 at each step,
+so the next sequence is 0 0 0 0. This means you have enough information to extrapolate the history!
+Visually, these sequences can be arranged like this:
+
+0   3   6   9  12  15
+  3   3   3   3   3
+    0   0   0   0
+
+To extrapolate, start by adding a new zero to the end of your list of zeroes; because the zeroes
+represent differences between the two values above them, this also means there is now a placeholder
+in every sequence above it:
+
+0   3   6   9  12  15   B
+  3   3   3   3   3   A
+    0   0   0   0   0
+
+You can then start filling in placeholders from the bottom up. A needs to be the result of
+increasing 3 (the value to its left) by 0 (the value below it); this means A must be 3:
+
+0   3   6   9  12  15   B
+  3   3   3   3   3   3
+    0   0   0   0   0
+
+Finally, you can fill in B, which needs to be the result of increasing 15 (the value to its left) by
+3 (the value below it), or 18:
+
+0   3   6   9  12  15  18
+  3   3   3   3   3   3
+    0   0   0   0   0
+
+So, the next value of the first history is 18.
+
+Finding all-zero differences for the second history requires an additional sequence:
+
+1   3   6  10  15  21
+  2   3   4   5   6
+    1   1   1   1
+      0   0   0
+
+Then, following the same process as before, work out the next value in each sequence from the bottom
+up:
+
+1   3   6  10  15  21  28
+  2   3   4   5   6   7
+    1   1   1   1   1
+      0   0   0   0
+
+So, the next value of the second history is 28.
+
+The third history requires even more sequences, but its next value can be found the same way:
+
+10  13  16  21  30  45  68
+   3   3   5   9  15  23
+     0   2   4   6   8
+       2   2   2   2
+         0   0   0
+
+So, the next value of the third history is 68.
+
+If you find the next value for each history in this example and add them together, you get 114.
+
+Analyze your OASIS report and extrapolate the next value for each history. What is the sum of these
+extrapolated values?
+"""
+
+from pathlib import Path
+
+CUR_DIR = Path(__file__).parent.resolve()
+input_data_file = CUR_DIR / "input.txt"
+
+
+def parse(text: str) -> list[list[int]]:
+    return [list(map(int, line.split())) for line in text.splitlines()]
+
+
+def _calc_line(sequence: list[int]) -> int:
+    steps: list[list[int]] = [sequence]
+    i_step: int = 0
+
+    while True:
+        current_step = steps[i_step]
+        new_step = []
+        i_sequence: int = 1
+
+        while i_sequence < len(current_step):
+            new_step.append(current_step[i_sequence] - current_step[i_sequence - 1])
+            i_sequence += 1
+
+        for number in new_step:
+            if number != 0:
+                steps.append(new_step)
+                i_step += 1
+                break
+        else:
+            break
+
+    last_number = 0
+    for step in steps[::-1]:
+        last_number += step[-1]
+
+    return last_number
+
+
+def algo(input_data: list[list[int]]) -> int:
+    result = 0
+    for sequence in input_data:
+        result += _calc_line(sequence)
+    return result
+
+
+def start(intput: str) -> int:
+    return algo(parse(intput))
+
+
+def main():
+    print("first... ")
+    assert 114 == start(
+        """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
+    )
+    print("ok")
+
+    print("second... ")
+    text = input_data_file.read_text()
+    print(start(text))
+    # print("expected: 19241")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/advent_of_code/year_2023/day_9/star_2/algo.py
@@ -0,0 +1,90 @@
+"""
+For each history, repeat the process of finding differences until the sequence of differences is
+entirely zero. Then, rather than adding a zero to the end and filling in the next values of each
+previous sequence, you should instead add a zero to the beginning of your sequence of zeroes, then
+fill in new first values for each previous sequence.
+
+In particular, here is what the third example history looks like when extrapolating back in time:
+
+5  10  13  16  21  30  45
+  5   3   3   5   9  15
+   -2   0   2   4   6
+      2   2   2   2
+        0   0   0
+
+Adding the new values on the left side of each sequence from bottom to top eventually reveals the
+new left-most history value: 5.
+
+Doing this for the remaining example data above results in previous values of -3 for the first
+history and 0 for the second history. Adding all three new values together produces 2.
+
+Analyze your OASIS report again, this time extrapolating the previous value for each history. What
+is the sum of these extrapolated values?
+"""
+
+from pathlib import Path
+
+CUR_DIR = Path(__file__).parent.resolve()
+input_data_file = CUR_DIR / "input.txt"
+
+
+def parse(text: str) -> list[list[int]]:
+    return [list(map(int, line.split())) for line in text.splitlines()]
+
+
+def _calc_line(sequence: list[int]) -> int:
+    steps: list[list[int]] = [sequence]
+    i_step: int = 0
+
+    while True:
+        current_step = steps[i_step]
+        new_step = []
+        i_sequence: int = 1
+
+        while i_sequence < len(current_step):
+            new_step.append(current_step[i_sequence] - current_step[i_sequence - 1])
+            i_sequence += 1
+
+        for number in new_step:
+            if number != 0:
+                steps.append(new_step)
+                i_step += 1
+                break
+        else:
+            break
+
+    last_number = 0
+    for step in steps[::-1]:
+        last_number += step[-1]
+
+    return last_number
+
+
+def algo(input_data: list[list[int]]) -> int:
+    result = 0
+    for sequence in input_data:
+        result += _calc_line(sequence)
+    return result
+
+
+def start(intput: str) -> int:
+    return algo(parse(intput))
+
+
+def main():
+    print("first... ")
+    assert 114 == start(
+        """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
+    )
+    print("ok")
+
+    print("second... ")
+    text = input_data_file.read_text()
+    print(start(text))
+    # print("expected: 19241")
+
+
+if __name__ == "__main__":
+    main()
